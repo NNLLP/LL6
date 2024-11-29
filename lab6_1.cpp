bfaf1d78e1d5ac34bf17f0ffd524ca82ba58913d
NNLLP <98796613+NNLLP@users.noreply.github.com>
2024-11-29 15:38:25 +0700

    lab6_1.cpp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i=1;
+    int evenn=0;
+    int oddn=0;
+    while(i != 0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        if(i % 2 == 0 && i != 0){
+            evenn += 1;
+        }
+        else if(i != 0){
+            oddn += 1;
+        }
+    }
+    cout << "#Even numbers = " << evenn << "\n";
+    cout << "#Odd numbers = " << oddn;
     return 0;
 }
